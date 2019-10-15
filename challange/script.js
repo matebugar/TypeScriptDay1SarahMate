29a87f12bda6c499d93214a1eee80393e24db063
Sarah E <egger.sarah@ymail.com>
2019-10-15 15:45:52 +0200

    Done with presentation

--- challange/script.js
+++ challange/script.js
@@ -5,3 +5,5 @@ for (var i = 0; i < menuu.length; i++) {
         "<div class=\"col-lg-3 col-md-6 mb-4\">\n        <div class=\"card h-100\">\n          <img class=\"card-img-top\" src=\"" + menuu[i].image + "\" alt=\"\">\n          <div class=\"card-body\">\n            <h4 class=\"card-title\">" + menuu[i].name + "</h4>\n            <p class=\"card-text\">" + menuu[i].description + "</p>\n          </div>\n          <div class=\"card-footer\">\n            <p>Rating: " + menuu[i].rating + "</p>\n          </div>\n        </div>\n      </div>";
 }
 ;
+
+
