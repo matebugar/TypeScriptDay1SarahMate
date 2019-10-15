6e0a35962689aba98982fe94f2463b39237b2440
Sarah E <egger.sarah@ymail.com>
2019-10-15 14:17:50 +0200

    Done with menu page and JSON

--- challange/script.js
+++ challange/script.js
@@ -1,5 +1,7 @@
-var menu = JSON.parse(dishes);
-for (i = 0; i < menu.length; i++) {
-    documentgetElementById("dishDisplay").innerHTML += ("");
+var menuu = dishes;
+console.table(dishes);
+for (var i = 0; i < menuu.length; i++) {
+    document.getElementById("dishDisplay").innerHTML +=
+        "<div class=\"col-lg-3 col-md-6 mb-4\">\n        <div class=\"card h-100\">\n          <img class=\"card-img-top\" src=\"" + menuu[i].image + "\" alt=\"\">\n          <div class=\"card-body\">\n            <h4 class=\"card-title\">" + menuu[i].name + "</h4>\n            <p class=\"card-text\">" + menuu[i].description + "</p>\n          </div>\n          <div class=\"card-footer\">\n            <p>Rating: " + menuu[i].rating + "</p>\n          </div>\n        </div>\n      </div>";
 }
 ;
--- challange/script.ts
+++ challange/script.ts
@@ -1,4 +1,18 @@
-var menu = JSON.parse (dishes); 
-for (i=0; i< menu.length; i++) {
-	documentgetElementById("dishDisplay").innerHTML += ("");
+var menuu : Array<{image: string,name: string, description: string, rating: number}> = dishes;
+console.table(dishes);
+
+for (let i=0; i< menuu.length; i++) {
+	document.getElementById("dishDisplay").innerHTML += 
+		`<div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="${menuu[i].image}" alt="">
+          <div class="card-body">
+            <h4 class="card-title">${menuu[i].name}</h4>
+            <p class="card-text">${menuu[i].description}</p>
+          </div>
+          <div class="card-footer">
+            <p>Rating: ${menuu[i].rating}</p>
+          </div>
+        </div>
+      </div>`;
 };
